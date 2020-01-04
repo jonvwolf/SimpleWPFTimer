fdfc4ab58238dd5d544c7f27b2a78155044b8846
osakahq <5056636+osakahq@users.noreply.github.com>
2020-01-03 20:44:33 -0600

    Added user control for the clock UI Added hotkeys for switching tabs

---
+++ SimpleTimer/ClockUserCtrl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleTimer
+{
+    /// <summary>
+    /// Interaction logic for ClockUserCtrl.xaml
+    /// </summary>
+    public partial class ClockUserCtrl : UserControl
+    {
+        public enum Mode
+        {
+            Timer, Stopwatch
+        }
+        public ClockUserCtrl(Mode mode)
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SimpleTimer/IClock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SimpleTimer
+{
+    public interface IClock : INotifyPropertyChanged
+    {
+        string Text { get; set; }
+        void PrimaryButton();
+        void SecondaryButton();
+        void FocusText();
+    }
+}
