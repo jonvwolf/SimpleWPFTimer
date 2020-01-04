4bcfedcef3068f0c65df28a6f7ffd21cbdbc1c08
osakahq <5056636+osakahq@users.noreply.github.com>
2020-01-03 20:44:33 -0600

    Added user control for the clock UI Added hotkeys for switching tabs

--- SimpleTimer/MainWindow.xaml.cs
+++ SimpleTimer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,14 +21,56 @@ namespace SimpleTimer
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly ClockUserCtrl _timer;
+        readonly ClockUserCtrl _stopwatch;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            _timer = new ClockUserCtrl(ClockUserCtrl.Mode.Timer);
+            _stopwatch = new ClockUserCtrl(ClockUserCtrl.Mode.Stopwatch);
+
+            TimerContentCtrl.Content = _timer;
+            StopwatchContentCtrl.Content = _stopwatch;
+
+            RoutedCommand tabRightHotKeyCommand = new RoutedCommand();
+            tabRightHotKeyCommand.InputGestures.Add(new KeyGesture(Key.Right, ModifierKeys.Alt));
+            CommandBindings.Add(new CommandBinding(tabRightHotKeyCommand, TabRightHotKey));
+
+            RoutedCommand tabLeftHotKeyCommand = new RoutedCommand();
+            tabLeftHotKeyCommand.InputGestures.Add(new KeyGesture(Key.Left, ModifierKeys.Alt));
+            CommandBindings.Add(new CommandBinding(tabLeftHotKeyCommand, TabLeftHotKey));
         }
 
-        private void btnStart_Click(object sender, RoutedEventArgs e)
+        private void TabRightHotKey(object sender, ExecutedRoutedEventArgs e)
         {
-
+            SwitchTabs(true);
         }
+
+        private void TabLeftHotKey(object sender, ExecutedRoutedEventArgs e)
+        {
+            SwitchTabs(false);
+        }
+
+        private void SwitchTabs(bool right)
+        {
+            if (right)
+            {
+                if (tabCtrl.SelectedIndex == (tabCtrl.Items.Count - 1))
+                    tabCtrl.SelectedIndex = 0;
+                else
+                    tabCtrl.SelectedIndex++;
+            }
+            else
+            {
+                if (tabCtrl.SelectedIndex == 0)
+                    tabCtrl.SelectedIndex = tabCtrl.Items.Count - 1;
+                else
+                    tabCtrl.SelectedIndex--;
+            }
+            
+        }
+
     }
 }
