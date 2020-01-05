55049ea0df44af9f6ff62d5c0627a1bbf966ca42
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2020-01-05 12:09:17 -0600

    Fixed key down number when text already has focus Added return binding WIP clocks impl

---
+++ SimpleTimer/StopwatchClock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SimpleTimer
+{
+    public class StopwatchClock : IClock
+    {
+        public string Text { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void FocusText()
+        {
+            
+        }
+
+        public void PrimaryButton()
+        {
+            
+        }
+
+        public void SecondaryButton()
+        {
+            
+        }
+    }
+}
