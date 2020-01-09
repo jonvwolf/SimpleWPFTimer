3fa6a124490c3d9be8b74826ff89ca5add755139
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2020-01-09 15:01:28 -0600

    Added helper method to load resources Changed catch all exceptions code policy to suggestion Change code for sound loading and dispose

---
+++ SimpleTimer/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Packaging;
+using System.Text;
+using System.Windows;
+using System.Windows.Resources;
+
+namespace SimpleTimer
+{
+    public static class Utils
+    {
+        public static Stream GetResourceStream(string resourceName)
+        {
+            string resourcePath = "resources/" + resourceName;
+            string url = string.Format(
+                null,
+                "pack://application:,,,/{0};component//{1}",
+                "SimpleTimer",
+                resourcePath);
+
+            try
+            {
+                string s = PackUriHelper.UriSchemePack;
+                var uri = new Uri(url);
+                StreamResourceInfo sri = Application.GetResourceStream(uri);
+                return sri.Stream;
+            }
+            catch (Exception)
+            {
+                //TODO log
+                return null;
+            }
+        }
+    }
+}
