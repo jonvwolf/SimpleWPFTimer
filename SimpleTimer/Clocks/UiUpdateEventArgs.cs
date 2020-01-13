34fb9c7aef7d1c778a3c64d9a0f4269292902e09
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2020-01-12 23:20:39 -0600

    Code cleanup/reorganizing Added switch tabs -> pause clock Fixed enter hotkey.. now it's shift+enter

--- SimpleTimer/Clocks/UiUpdateEventArgs.cs
+++ SimpleTimer/Clocks/UiUpdateEventArgs.cs
@@ -7,6 +7,10 @@ namespace SimpleTimer
 {
     public class UiUpdatedEventArgs : EventArgs
     {
+        public enum PrimaryButtonMode
+        {
+            Stopped, Running
+        }
         public PrimaryButtonMode? PrimaryBtn { get; set; }
         public TimeSpan? Left { get; set; }
     }
