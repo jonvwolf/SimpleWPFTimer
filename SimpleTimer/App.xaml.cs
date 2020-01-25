17d4469b389ce789307cd0da2c3ae524aa79a54d
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2020-01-25 17:21:41 -0600

    Added flush for logger Added log for unhandled exceptions Logger initializes at App now

--- SimpleTimer/App.xaml.cs
+++ SimpleTimer/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Serilog;
+using System.Windows;
 
 namespace SimpleTimer
 {
@@ -7,5 +8,25 @@ namespace SimpleTimer
     /// </summary>
     public partial class App : Application
     {
+        public App() : base()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Error()
+                .WriteTo.File("errorlog.txt", rollingInterval: RollingInterval.Month, retainedFileCountLimit: 3, fileSizeLimitBytes: 1048576)
+                .CreateLogger();
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Log.Logger.Error($"Unhandled app exception: {e.Exception.ToString()}");
+            Log.CloseAndFlush();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            Log.CloseAndFlush();
+        }
     }
 }
