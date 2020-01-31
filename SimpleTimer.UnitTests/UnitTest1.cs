d6830702dee8582593f06b78a84b84875a4d3dd7
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2020-01-31 16:57:01 -0600

    test window open

--- SimpleTimer.UnitTests/UnitTest1.cs
+++ SimpleTimer.UnitTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using SimpleTimer.ClockUserControls;
 using System;
+using System.Threading;
 using Xunit;
 
 namespace SimpleTimer.UnitTests
@@ -11,6 +12,24 @@ namespace SimpleTimer.UnitTests
         {
             var ok = new UIEventArgs(UIEventArgs.UIEventType.BtnResetClicked);
             Assert.Equal(UIEventArgs.UIEventType.BtnResetClicked, ok.Type);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            MainWindow window = null;
+            var thread = new Thread(() =>
+            {
+                window = new MainWindow();
+                window.Closed += (s, e) => window.Dispatcher.InvokeShutdown();
+                window.Show();
+                Thread.Sleep(1000);
+                window.Close();
+                System.Windows.Threading.Dispatcher.Run();
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
         }
     }
 }
