1b704e4b27954889c6f6f7eaf7e6486b0a3eef9d
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2020-02-04 23:24:44 -0600

    Finished integration testing Added uid and differentation between user controls Fixed reset btn in stopwatch

--- SimpleTimer.WinAppDriverTests/AppSession.cs
+++ SimpleTimer.WinAppDriverTests/AppSession.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium.Remote;
@@ -7,78 +8,70 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
-using Xunit;
-using Xunit.Abstractions;
 
 namespace SimpleTimer.WinAppDriverTests
 {
     public class AppSession : IDisposable
     {
         private const string WinAppDriverUrl = "http://127.0.0.1:4723";
-        private static WindowsDriver<WindowsElement> _session { get; set; }
-        private readonly static object _lock = new object();
-        public WindowsDriver<WindowsElement> Session { get => _session; }
-        public Dictionary<string, WindowsElement> Elements { get; private set; } = new Dictionary<string, WindowsElement>();
+        
+        public WindowsDriver<WindowsElement> Session { get; private set; }
+        public string FullExePath { get; private set; }
         public AppSession()
         {
             Setup();
         }
 
-        protected void Setup()
+        private void Setup()
         {
-            lock (_lock)
+            if (Session == null)
             {
-                if (_session == null)
+                string appId = "";
+                string folder = @"..\..\..\..\";
+                string[] filesFound = Directory.GetFiles(folder, "*SimpleTimer.exe", SearchOption.AllDirectories);
+                if (filesFound.Length == 0)
                 {
-                    string appId = "";
-                    string folder = @"..\..\..\..\";
-                    string[] filesFound = Directory.GetFiles(folder, "*SimpleTimer.exe", SearchOption.AllDirectories);
-                    if (filesFound.Length == 0)
+                    throw new InvalidOperationException("Not able to found SimpleTimer.exe file");
+                }
+                if (filesFound.Length >= 2)
+                {
+                    foreach (var file in filesFound)
                     {
-                        throw new InvalidOperationException("Not able to found SimpleTimer.exe file");
-                    }
-                    if (filesFound.Length >= 2)
-                    {
-                        foreach (var file in filesFound)
+                        if (file.Contains(@"SimpleTimer" + Path.DirectorySeparatorChar + "bin", StringComparison.OrdinalIgnoreCase))
                         {
-                            if (file.Contains("bin", StringComparison.OrdinalIgnoreCase))
+                            if (file.Contains("debug", StringComparison.OrdinalIgnoreCase) || file.Contains("release", StringComparison.OrdinalIgnoreCase))
                             {
-                                if (file.Contains("debug", StringComparison.OrdinalIgnoreCase) || file.Contains("release", StringComparison.OrdinalIgnoreCase))
-                                {
-                                    appId = Path.GetFullPath(file);
-                                }
+                                appId = Path.GetFullPath(file);
                             }
                         }
                     }
-                    if (string.IsNullOrEmpty(appId))
-                    {
-                        throw new InvalidOperationException("No SimpleTimer.exe found in a bin/debug or bin/release folder");
-                    }
-                    appId = @"D:\a\r1\a\_jonwolfdev_SimpleWPFTimer\SimpleTimer\bin\Debug\netcoreapp3.1\SimpleTimer.exe";
-                    var options = new AppiumOptions();
-                    options.AddAdditionalCapability("app", appId);
-
-                    _session = new WindowsDriver<WindowsElement>(new Uri(WinAppDriverUrl), options);
-
-                    Assert.Contains("Simple Timer", _session.Title);
-                    Assert.NotNull(_session);
-                    Assert.NotNull(_session.SessionId);
-
-                    _session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
                 }
+                if (string.IsNullOrEmpty(appId))
+                {
+                    throw new InvalidOperationException("No SimpleTimer.exe found in a bin/debug or bin/release folder");
+                }
+
+                var options = new AppiumOptions();
+                options.AddAdditionalCapability("app", appId);
+
+                Session = new WindowsDriver<WindowsElement>(new Uri(WinAppDriverUrl), options);
+
+                Assert.AreEqual("Simple Timer", Session.Title);
+                Assert.IsNotNull(Session);
+                Assert.IsNotNull(Session.SessionId);
+
+                Session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+                FullExePath = appId;
             }
         }
 
-        protected void Shutdown()
+        private void Shutdown()
         {
-            lock (_lock)
-            {
-                if (_session == null)
-                    return;
-                _session.Close();
-                _session.Quit();
-                _session = null;
-            }
+            if (Session == null)
+                return;
+            Session.Close();
+            Session.Quit();
+            Session = null;
         }
 
         #region IDisposable Support
--- SimpleTimer.WinAppDriverTests/HappyPathTests.cs
+++ SimpleTimer.WinAppDriverTests/HappyPathTests.cs
@@ -1,74 +1,106 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Support.UI;
 using System;
+using System.IO;
 using System.Threading;
-using Xunit;
-using Xunit.Abstractions;
 
 namespace SimpleTimer.WinAppDriverTests
 {
-    public class HappyPathTests : IClassFixture<AppSession>
+    [TestClass]
+    public class HappyPathTests
     {
-        AppSession _app;
-        WindowsElement _timerTextBox;
-        WindowsElement _timerStartButton;
-        WindowsElement _timerResetButton;
-        public HappyPathTests(AppSession app)
+        [TestMethod]
+        public void Test0_InitialValues()
         {
-            _app = app;
-            
-            _timerTextBox = _app.Session.FindElementByClassName("TextBox");
-            var buttons = _app.Session.FindElementsByClassName("Button");
-            foreach(var button in buttons)
+            string dir = "";
+            using (var app = new AppSession())
             {
-                if (button.Text == "Start")
-                {
-                    _timerStartButton = button;
-                }
-                else if (button.Text == "Reset")
-                {
-                    _timerResetButton = button;
-                }
+                dir = app.FullExePath;
+                var mainWindow = app.Session.FindElementByAccessibilityId("MainWindow");
+                //first test
+                //arrange
+                WebDriverWait wait = new WebDriverWait(app.Session, TimeSpan.FromMilliseconds(1200));
+                
+                var timerTextBox = app.Session.FindElementByAccessibilityId("TextTime_timer");
+                var timerStartButton = app.Session.FindElementByAccessibilityId("StartButton_timer");
+                var timerResetButton = app.Session.FindElementByAccessibilityId("ResetButton_timer");
+
+                //act
+
+                //assert
+                Assert.AreEqual("00:00:00", timerTextBox.Text);
+                Assert.AreEqual("Start", timerStartButton.Text);
+                Assert.AreEqual("Reset", timerResetButton.Text);
+
+                //second test
+
+                //arrange
+                string buttonText = "";
+                string okText = "";
+                string startText = "";
+                string okResetText = "";
+                
+                //act
+                timerTextBox.SendKeys(Keys.Control + "a" + Keys.Control);
+                timerTextBox.SendKeys(Keys.Delete);
+                timerTextBox.SendKeys("01");
+
+                timerStartButton.Click();
+                wait.Until(x => { return timerStartButton.Text.Contains("Stop"); });
+                buttonText = timerStartButton.Text;
+
+                wait.Until(x => { return timerStartButton.Text.Contains("OK"); });
+                okText = timerStartButton.Text;
+                timerStartButton.Click();
+
+                wait.Until(x => { return timerStartButton.Text.Contains("Start"); });
+
+                startText = timerStartButton.Text;
+
+                timerResetButton.Click();
+                wait.Until(x => { return timerStartButton.Text.Contains("OK"); });
+                okResetText = timerStartButton.Text;
+                timerStartButton.Click();
+
+                timerTextBox.SendKeys(Keys.Control + "a" + Keys.Control);
+                timerTextBox.SendKeys(Keys.Delete);
+                timerTextBox.SendKeys("01");
+
+                //assert
+                Assert.AreEqual("Stop", buttonText);
+                Assert.AreEqual("OK", okText);
+                Assert.AreEqual("Start", startText);
+                Assert.AreEqual("OK", okResetText);
+
+
+                //third test
+                mainWindow.SendKeys(Keys.Alt + Keys.Right + Keys.Alt);
+                WebDriverWait secondWait = new WebDriverWait(app.Session, TimeSpan.FromMilliseconds(2000));
+
+                WindowsElement swTextBox = app.Session.FindElementByAccessibilityId("TextTime_sw");
+                WindowsElement swStartButton = app.Session.FindElementByAccessibilityId("StartButton_sw");
+                WindowsElement swResetButton = app.Session.FindElementByAccessibilityId("ResetButton_sw");
+
+                secondWait.Until(x => { return !swTextBox.Enabled && swTextBox.Displayed; });
+
+                Assert.AreEqual("00:00:00", swTextBox.Text);
+
+                swStartButton.Click();
+                secondWait.Until(x => { return swTextBox.Text.Contains("00:00:01"); });
+
+                swResetButton.Click();
+                secondWait.Until(x => { return swTextBox.Text.Contains("00:00:00"); });
+
+                swStartButton.Click();
+                secondWait.Until(x => { return swStartButton.Text.Contains("Start"); });
             }
-        }
-        [Fact]
-        public void Test1_InitialValues()
-        {
-            //arrange
-            
-            //act
 
-            //assert
-            Assert.Equal("00:00:00", _timerTextBox.Text);
-            Assert.Equal("Start", _timerStartButton.Text);
-            Assert.Equal("Reset", _timerResetButton.Text);
+            dir = Path.GetDirectoryName(dir);
+            string[] files = Directory.GetFiles(dir, "*errorlog*", SearchOption.AllDirectories);
+            Assert.AreEqual(0, files.Length);
         }
 
-        [Fact]
-        public void Test2_TimerWorks()
-        {
-            //arrange
-            string buttonText = "";
-            string okText = "";
-            string startText = "";
-            //act
-            _timerTextBox.SendKeys(Keys.Control + "a" + Keys.Control);
-            _timerTextBox.SendKeys(Keys.Delete);
-            _timerTextBox.SendKeys("01");
-            
-            _timerStartButton.Click();
-            Thread.Sleep(100);
-            buttonText = _timerStartButton.Text;
-            Thread.Sleep(1100);
-            okText = _timerStartButton.Text;
-            _timerStartButton.Click();
-            Thread.Sleep(10);
-            startText = _timerStartButton.Text;
-
-            //assert
-            Assert.Equal("Stop", buttonText);
-            Assert.Equal("OK", okText);
-            Assert.Equal("Start", startText);
-        }
     }
 }
